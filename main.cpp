7b05aba569fdbb5fb27f5048cf20238513860721
3t13nn3 <etiennepenault1997@gmail.com>
2019-11-29 19:54:34 +0100

    Debug Cross Platform

--- main.cpp
+++ main.cpp
@@ -265,11 +265,9 @@ int main(int argc, char ** argv){
             std::vector<int> to_wait = r.GetWaitTimes();
 
             /*CALCULATING CURRENT POSITION OF OUR CURRENT CAR IN FUNCTION OF TIME AND DEPARTUR/ARRIVAL NODES*/
-            int tmp(std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now()-starts[i]).count());
+            //int tmp(std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now()-starts[i]).count());
             
-        #ifdef __APPLE__
-            tmp(std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::steady_clock::now() - starts[i]).count());
-        #endif
+            int tmp(std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::high_resolution_clock::now()-starts[i]).count());
 
             float ratio = (float)tmp/to_wait[i];
             point a = points[car_nodes[i]] * (1 - ratio);
